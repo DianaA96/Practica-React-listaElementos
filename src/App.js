de2752f7cf9a956618cc12cc6183a305e9de3d83
Diana Aguirre <>
2021-02-28 22:34:20 -0600

    Archivo App.js proporcionado en clase, contiene un objeto que se pasa como props a la tarjeta del tamalito. Ejemplo de azúcar sintáctica

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css";
+import Card from './Card'
+
 
 function App() {
+  let result = {
+    status: 200,
+    statusText: "Success",
+    data: [
+      { details: "dulce", name: "Tamal de dulce", price: "$12.00" },
+      { details: "picante", name: "Tamal verde", price: "$10.00" },
+      { details: "picante", name: "Tamal rojo", price: "$10.50" },
+      { details: "picante", name: "Tamal de rajas", price: "$11.00" }
+    ]
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <React.Fragment>
+      <header>
+        <h1>Tamalitos</h1>
       </header>
-    </div>
-  );
+
+      <section className="losTamalitos">
+        <Card{...result}/>
+      </section>
+    </React.Fragment>
+    );
 }
 
 export default App;
